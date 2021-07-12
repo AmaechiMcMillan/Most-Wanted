bc850b9e8ce63a5eba253342a374870ed1f2ac10
AmaechiMcMillan <86973092+AmaechiMcMillan@users.noreply.github.com>
2021-07-12 08:38:13 -0700

    Deleted old code to add better working functions.

--- mostWanted.js
+++ mostWanted.js
@@ -1,434 +1,66 @@
 "use strict"
 
-
-let userInput = prompt("Male or Female?");
-let genderSearch = userInput;
-function selectGender(array){
-    if (userInput === "male"){
-    }
-    else{
-        (userInput === "female")
-    }
-    return genderSearch;
-}
-console.log(genderSearch);
-
-
-let userInputGender = prompt("Choose male or Female?");
-data.filter(function isMale(data){
-    return data === "male";
-    console.log(data);
-});
-data.filter(function isFemale(data){
-    return data === "female";
-    console.log(data);
-});
-
-//search for gender, height, weight, eye color
-
-function searchMultipleCriteria(array{
-	let userInputCriteria = prompt("Press 1 for Eye Color: \n Press 2 for Height: \n Press 3 for Weight: \n Press 4 for Gender:")
-	let press1 = "eyeColor";
-	let press2 = "height";
-	let press3 = "weight";
-	let press4 = "gender";
-	switch(userInputCriteria){
-    		case 1: eyeColor = "brown, black, hazel, blue, green"
-    		break;
-   		case 2: height = "58 =< 80"
-   		break;
-    		case 3: weight = "100 =< 200"
-   		break;
-    		case 4: gender = "male, female"
-    		break;
+//allow user to search based on a single criteria, male or female
+function app(people){
+	let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
+	let searchResults;
+	switch(searchType){
+	  case 'yes':
+		searchResults = searchByName(people);
+		break;
+	  case 'no':
+		// TODO: search by traits
+		break;
+		default:
+	  app(people); // restart app
+		break;
+	}
+	mainMenu(searchResults, people);
 }
 
+//allow user to search for multiple traits like gender, height, weight, eye color and occupation
+function searchByName(people){
+	let firstName = promptFor("What is the person's first name?", autoValid);
+	let lastName = promptFor("What is the person's last name?", autoValid);
+  
+	let foundPerson = people.filter(function(potentialMatch){
+	  if(potentialMatch.firstName === firstName && potentialMatch.lastName === lastName){
+		return true;
+	  }
+	  else{
+		return false;
+	  }
+	})
+	// TODO: find the person single person object using the name they entered.
+	return foundPerson;
+  }
 
 
+function searchByEyeColor(){
+	let eyeColor = promptFor("What is the person's eye color?", autoValid);
+  
+	let foundPerson = people.filter(function(potentialMatch){
+	  if(potentialMatch.eyeColor === eyeColor){
+		return true;
+	  }
+	  else{
+		return false;
+	  }
+	})
+	// TODO: find the person single person object using the name they entered.
+	return foundPerson;
+}
 
-
-
-
-
-
-
-
-// const data = [
-// 	{
-// 		"id": 272822514,
-// 		"firstName": "Billy",
-// 		"lastName": "Bob",
-// 		"gender": "male",
-// 		"dob": "1/18/1949",
-// 		"height": 71,
-// 		"weight": 175,
-// 		"eyeColor": "brown",
-// 		"occupation": "programmer",
-// 		"parents": [],
-// 		"currentSpouse": 401222887
-// 	},
-// 	{
-// 		"id": 401222887,
-// 		"firstName": "Uma",
-// 		"lastName": "Bob",
-// 		"gender": "female",
-// 		"dob": "4/1/1947",
-// 		"height": 65,
-// 		"weight": 162,
-// 		"eyeColor": "brown",
-// 		"occupation": "assistant",
-// 		"parents": [],
-// 		"currentSpouse": 272822514
-// 	},
-// 	{
-// 		"id": 409574486,
-// 		"firstName": "Michael",
-// 		"lastName": "Walkens",
-// 		"gender": "male",
-// 		"dob": "5/9/1951",
-// 		"height": 76,
-// 		"weight": 250,
-// 		"eyeColor": "brown",
-// 		"occupation": "landscaper",
-// 		"parents": [],
-// 		"currentSpouse": 260451248
-// 	},
-// 	{
-// 		"id": 260451248,
-// 		"firstName": "Jon",
-// 		"lastName": "Walkens",
-// 		"gender": "male",
-// 		"dob": "9/6/1945",
-// 		"height": 62,
-// 		"weight": 115,
-// 		"eyeColor": "brown",
-// 		"occupation": "assistant",
-// 		"parents": [],
-// 		"currentSpouse": 409574486
-// 	},
-// 	{
-// 		"id": 629807187,
-// 		"firstName": "Jack",
-// 		"lastName": "Pafoy",
-// 		"gender": "male",
-// 		"dob": "3/16/1938",
-// 		"height": 70,
-// 		"weight": 207,
-// 		"eyeColor": "black",
-// 		"occupation": "nurse",
-// 		"parents": [],
-// 		"currentSpouse": 464142841
-// 	},
-// 	{
-// 		"id": 464142841,
-// 		"firstName": "Jen",
-// 		"lastName": "Pafoy",
-// 		"gender": "female",
-// 		"dob": "4/10/1940",
-// 		"height": 72,
-// 		"weight": 256,
-// 		"eyeColor": "black",
-// 		"occupation": "student",
-// 		"parents": [],
-// 		"currentSpouse": 629807187
-// 	},
-// 	{
-// 		"id": 982411429,
-// 		"firstName": "Mister",
-// 		"lastName": "Potatoo",
-// 		"gender": "male",
-// 		"dob": "12/18/1952",
-// 		"height": 66,
-// 		"weight": 170,
-// 		"eyeColor": "hazel",
-// 		"occupation": "architect",
-// 		"parents": [],
-// 		"currentSpouse": 595767575
-// 	},
-// 	{
-// 		"id": 595767575,
-// 		"firstName": "Missuz",
-// 		"lastName": "Potatoo",
-// 		"gender": "female",
-// 		"dob": "10/28/1948",
-// 		"height": 59,
-// 		"weight": 137,
-// 		"eyeColor": "blue",
-// 		"occupation": "architect",
-// 		"parents": [],
-// 		"currentSpouse": 982411429
-// 	},
-// 	{
-// 		"id": 693243224,
-// 		"firstName": "Joy",
-// 		"lastName": "Madden",
-// 		"gender": "female",
-// 		"dob": "4/20/1939",
-// 		"height": 69,
-// 		"weight": 199,
-// 		"eyeColor": "hazel",
-// 		"occupation": "doctor",
-// 		"parents": [],
-// 		"currentSpouse": null
-// 	},
-// 	{
-// 		"id": 888201200,
-// 		"firstName": "Mader",
-// 		"lastName": "Madden",
-// 		"gender": "male",
-// 		"dob": "5/6/1937",
-// 		"height": 76,
-// 		"weight": 205,
-// 		"eyeColor": "black",
-// 		"occupation": "landscaper",
-// 		"parents": [],
-// 		"currentSpouse": null
-// 	},
-// 	{
-// 		"id": 878013758,
-// 		"firstName": "Jill",
-// 		"lastName": "Pafoy",
-// 		"gender": "female",
-// 		"dob": "2/8/1972",
-// 		"height": 74,
-// 		"weight": 118,
-// 		"eyeColor": "brown",
-// 		"occupation": "programmer",
-// 		"parents": [401222887],
-// 		"currentSpouse": 294874671
-// 	},
-// 	{
-// 		"id": 951747547,
-// 		"firstName": "Ralph",
-// 		"lastName": "Bob",
-// 		"gender": "male",
-// 		"dob": "12/23/1969",
-// 		"height": 66,
-// 		"weight": 179,
-// 		"eyeColor": "blue",
-// 		"occupation": "nurse",
-// 		"parents": [401222887],
-// 		"currentSpouse": 159819275
-// 	},
-// 	{
-// 		"id": 159819275,
-// 		"firstName": "Jasmine",
-// 		"lastName": "Bob",
-// 		"gender": "female",
-// 		"dob": "12/18/1969",
-// 		"height": 58,
-// 		"weight": 156,
-// 		"eyeColor": "blue",
-// 		"occupation": "assistant",
-// 		"parents": [409574486, 260451248],
-// 		"currentSpouse": 951747547
-// 	},
-// 	{
-// 		"id": 348457184,
-// 		"firstName": "Annie",
-// 		"lastName": "Pafoy",
-// 		"gender": "female",
-// 		"dob": "11/4/1970",
-// 		"height": 62,
-// 		"weight": 235,
-// 		"eyeColor": "hazel",
-// 		"occupation": "landscaper",
-// 		"parents": [629807187, 464142841],
-// 		"currentSpouse": null
-// 	},
-// 	{
-// 		"id": 294874671,
-// 		"firstName": "Dave",
-// 		"lastName": "Pafoy",
-// 		"gender": "male",
-// 		"dob": "8/5/1967",
-// 		"height": 61,
-// 		"weight": 112,
-// 		"eyeColor": "green",
-// 		"occupation": "doctor",
-// 		"parents": [629807187, 464142841],
-// 		"currentSpouse": 878013758
-// 	},
-// 	{
-// 		"id": 931247228,
-// 		"firstName": "Amii",
-// 		"lastName": "Pafoy",
-// 		"gender": "female",
-// 		"dob": "3/13/1963",
-// 		"height": 74,
-// 		"weight": 184,
-// 		"eyeColor": "brown",
-// 		"occupation": "landscaper",
-// 		"parents": [629807187, 464142841],
-// 		"currentSpouse": null
-// 	},
-// 	{
-// 		"id": 822843554,
-// 		"firstName": "Regina",
-// 		"lastName": "Madden",
-// 		"gender": "female",
-// 		"dob": "7/26/1959",
-// 		"height": 71,
-// 		"weight": 249,
-// 		"eyeColor": "brown",
-// 		"occupation": "nurse",
-// 		"parents": [693243224, 888201200],
-// 		"currentSpouse": null
-// 	},
-// 	{
-// 		"id": 819168108,
-// 		"firstName": "Hana",
-// 		"lastName": "Madden",
-// 		"gender": "female",
-// 		"dob": "10/7/1953",
-// 		"height": 70,
-// 		"weight": 187,
-// 		"eyeColor": "brown",
-// 		"occupation": "politician",
-// 		"parents": [693243224, 888201200],
-// 		"currentSpouse": null
-// 	},
-// 	{
-// 		"id": 969837479,
-// 		"firstName": "Eloise",
-// 		"lastName": "Madden",
-// 		"gender": "female",
-// 		"dob": "12/11/1961",
-// 		"height": 63,
-// 		"weight": 241,
-// 		"eyeColor": "brown",
-// 		"occupation": "assistant",
-// 		"parents": [693243224, 888201200],
-// 		"currentSpouse": null
-// 	},
-// 	{
-// 		"id": 313207561,
-// 		"firstName": "Mattias",
-// 		"lastName": "Madden",
-// 		"gender": "male",
-// 		"dob": "2/19/1966",
-// 		"height": 70,
-// 		"weight": 110,
-// 		"eyeColor": "blue",
-// 		"occupation": "assistant",
-// 		"parents": [693243224, 888201200],
-// 		"currentSpouse": 313997561
-// 	},
-// 	{
-// 		"id": 313997561,
-// 		"firstName": "Ellen",
-// 		"lastName": "Madden",
-// 		"gender": "female",
-// 		"dob": "2/19/1970",
-// 		"height": 67,
-// 		"weight": 100,
-// 		"eyeColor": "blue",
-// 		"occupation": "doctor",
-// 		"parents": [],
-// 		"currentSpouse": 313207561
-// 	},
-// 	{
-// 		"id": 313998000,
-// 		"firstName": "Joey",
-// 		"lastName": "Madden",
-// 		"gender": "female",
-// 		"dob": "2/02/1987",
-// 		"height": 67,
-// 		"weight": 100,
-// 		"eyeColor": "blue",
-// 		"occupation": "doctor",
-// 		"parents": [313207561, 313997561],
-// 		"currentSpouse": null
-// 	}
-// ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Having the object
-// 1. Try to find all PerformanceObserverEntryList.
-//      Using the persons object's parents array, I want to look through all other(array.filter)
-//      
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-//   
-
-
-
-function findFamily(){
-
+function yesNo(input){
+  if(input.toLowerCase() == "yes" || input.toLowerCase() == "no"){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 
-
-
-// function makeDinner(meal = 'burritos'){
-//     console.log(`JJ is making ${meal} for dinner.`);
-// }
-// makeDinner();
-// makeDinner('naked chicken');
-
-
-// function doDishes(numberOfDishes){
-
-//     console.lof(`There are ${numberOfDishes} dishes left to clean`)
-
-//     if(numberOfDishes > 0){
-//         console.log('Cleaning a dish!');
-//         doDishes(numberOfDishes - 1);
-//     }
-//     else{
-//         console.log('All the dishes are clean.')
-//     }
-// }
-// doDishes(3);
-
-
-// function invest(totalInvested = 0){
-//     console.log(`Your total investments gains are: ${totalInvested}`);
-
-//     if(totalInvested > 100){
-//         console.log('You have reached your total investment gials. Withdrawing money...');
-//     }
-//     return totalInvested;
-// }   else {
-//         let newInvestmentAmount = 20;
-
-//         totalInvested = newInvestmentAmount * 2;
-//         return invest(totalInvested);
-//     }
-
-//     let moneyFromInvestments = invest();
-
-//     console.log(`Your total earnings from investments are: ${moneyFromInvestments}`);
-
-
-
-
-
-
-
+function autoValid(input){
+	return true; // default validation only
+  }
